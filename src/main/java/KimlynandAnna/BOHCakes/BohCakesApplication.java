0c383c7b2ce14bce1dcd49a395dd4ca4b9c070c2
Kimlyn De Lara <klyndelara@gmail.com>
2023-05-30 11:42:26 -0700

    Bohcakes Project

---
+++ src/main/java/KimlynandAnna/BOHCakes/BohCakesApplication.java
@@ -0,0 +1,13 @@
+package KimlynandAnna.BOHCakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BohCakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BohCakesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KimlynandAnna/BOHCakes/BohCakesApplicationTests.java
@@ -0,0 +1,13 @@
+package KimlynandAnna.BOHCakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BohCakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
