7e9d8785a9e64ac94d53602ca4e73ae743d35bff
Kimlyn De Lara <klyndelara@gmail.com>
2023-07-18 23:58:51 -0700

    Database adjustments

--- db/BOHcakes.sql
+++ db/BOHcakes.sql
@@ -1,10 +1,13 @@
- BEGIN TRANSACTION;
- drop table if exists cupcake, customer, sale;
+rollback;
+BEGIN TRANSACTION;
+
+
+
 
 CREATE TABLE cupcake(
 	cupcake_id serial not null,
-	name varchar (50) not null,
-	description varchar (100) not null,
+	name varchar(50) not null,
+	description varchar(200) not null,
 	cost varchar not null
 );
 
@@ -12,29 +15,36 @@ CREATE TABLE customer(
 	customer_id serial not null,
 	name varchar(25) not null,
 	phone_number varchar not null,
-	address varchar (50),
-	city varchar (25),
+	address varchar(50),
+	city varchar(25),
 	state varchar(2),
 	zipcode varchar (10)
 );
 
-/*CREATE TABLE sale(
+CREATE TABLE sale(
 	sale_id serial not null,
-	customer_id int not null,
-	sale_date date,
-	cupcake_id int not null
-);*/
+	sale_date date not null,
+	total_cost int not null
 
+);
+
+CREATE TABLE orders (
+	order_id serial not null,
+	customer_id int not null,
+	sale_id int,
+	cupcake_id  int not null,
+	quantity int not null
+);
 	
 INSERT INTO cupcake(name, description, cost) VALUES 
-	('Raspberry Cheesecake', 'Fresh Raspberry flavored cupcake, topped with a white chocolate cream frosting, drizzled with raspberry coulis, with a graham crack on topped.', '3.50'),
+	('Raspberry Cheesecake', 'Fresh Raspberry flavored cupcake, topped with a white chocolate cream frosting, drizzled with raspberry coulis, with a graham cracker on topped.', '3.50'),
 	('Mocha Espresso', 'Dark chocolate flavored cupcake, filled with espresso mocha mousse, topped with light vanilla buttercream, and sprinkle with espresso powder.', '3.00'),
-	('Vanilla Bliss', 'Vanilla chiffon cupcake, topped with a vanilla bavarian cream', '3.00'),
-	('Strawberry poptart','Strawberry and vanilla flavored cupcake, topped with strawberry bavarian cream and dusted with nilla wafers', '3.50'),
-	('Fruit Cocktial', 'Vanilla flavored cupcake, topped with choice of bavarian cream or cream cheese mousse and seasonal fruit on top', '4.00 or depends on type of fruit'),
-	('Miss Red Velvet', 'Simple red velet cake, topped with cream cheese mousse, and topped with a red macaroon', '3.00'),
-	('Lemon Drop', 'Lemon cupake, filled with lemon curd, topped with lemon vanilla buttercream, and candied lemon leaf', '3.00'),
-	('Hypno', 'Vanilla or chocolate cupcake infused with colors of the rainbow for a fun and colorful dessert experience, topped with choice of vanilla or chocolate mousse', '3.50');
+	('Vanilla Bliss', 'Vanilla chiffon cupcake, topped with a vanilla bavarian cream.', '3.00'),
+	('Strawberry poptart','Strawberry and vanilla flavored cupcake, topped with strawberry bavarian cream and dusted with nilla wafers.', '3.50'),
+	('Fruit Cocktial', 'Vanilla flavored cupcake, topped with choice of bavarian cream or cream cheese mousse and seasonal fruit on top.', '4.00 or depends on type of fruit'),
+	('Miss Red Velvet', 'Simple red velet cake, topped with cream cheese mousse, and topped with a red macaroon.', '3.00'),
+	('Lemon Drop', 'Lemon cupake, filled with lemon curd, topped with lemon vanilla buttercream, and candied lemon leaf.', '3.00'),
+	('Hypno', 'Vanilla or chocolate cupcake infused with colors of the rainbow for a fun and colorful dessert experience, topped with choice of vanilla or chocolate mousse.', '3.50');
 
 INSERT INTO customer(name, phone_number, address, city, state, zipcode) VALUES
 	('Sofia Valencia', '408-929-5568', '254 Evergreen Way', 'Tracy', 'CA', '95376'),
@@ -44,5 +54,23 @@ INSERT INTO customer(name, phone_number, address, city, state, zipcode) VALUES
 	('Leon Chris', '650-487-8991','5647 Jack Lantern Court','Emeryville', 'CA', '95647'),
 	('Crystal Espanosa', '714-658-9897', '4578 Vida Leon Drive','Santa Clara', 'CA', '95468');
 
-commit transaction;
+INSERT INTO sale(sale_date, total_cost) VALUES
+	('2023-01-02', 144.00),
+	('2023-01-21', 252.00),
+	('2023-02-18', 108.00),
+	('2023-02-19', 300.00),
+	('2023-03-25', 216.00),
+	('2023-04-25', 300.00),
+	('2023-05-13', 252.00),
+	('2023-05-14', 288.00),
+	('2023-05-20', 156.00),
+	('2023-06-03', 108.00),
+	('2023-06-10', 108.00);
 
+INSERT INTO order (customer_id, sale_id, cupcake_id, quantity) VALUES
+
+	
+
+commit; 
+
+select * from sale;
